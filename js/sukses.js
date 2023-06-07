64c3f3bb2e42679a7c6694ffd6a56dd5c865329f
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2023-06-07 17:41:15 +0700

    Update sukses.js

--- js/sukses.js
+++ js/sukses.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const fs = require('fs')
-global.sukses = `Sukses\nJangan lupa Follow yha\nInstagram : @vlntncptr\nYoutube : Lenttobs\n`
+global.sukses = `Sukses\nJangan lupa Follow yha\nInstagram : @lans.xys\nGithub : Lans-ofc\n`
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
